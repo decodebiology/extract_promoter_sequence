c57a93f72d39e6bb4ef269ba4dc8eec6400e9ab4
decodebiology <santhilalsubhash@gmail.com>
2023-09-13 15:07:29 -0400

    Updated functions

--- 1.GTFtoTable.R
+++ 1.GTFtoTable.R
@@ -2,19 +2,29 @@
 #hg38.fa : Genome used for alignment purpose
 #Homo_sapiens.GRCh38.84.chr_complete_annotation.txt : Gene annotations used for quantification purpose (extract from GTF using R script)
 #DE_genes_list_total.txt : Differentially expressed genes for whcih promoter to be extracted
-
+## Run: Rscript 1.GTFtoTable.R ensembl[OR 'gencode'] https://ftp.ensembl.org/pub/release-84/gtf/homo_sapiens/Homo_sapiens.GRCh38.84.chr.gtf.gz
 ## Prepare gene annotation file (necessary to obtain chromosomal lcoations of the gene)
 ## R script (Start)
-#gtf <- rtracklayer::import('/grid/beyaz/home/subhash/projects/endometrium_proj/bulk/annotation/Homo_sapiens.GRCh38.84.chr.gtf')
-gtf <- rtracklayer::import('https://ftp.ensembl.org/pub/release-84/gtf/homo_sapiens/Homo_sapiens.GRCh38.84.chr.gtf.gz')
+
+args = commandArgs(trailingOnly=TRUE)
+gtf_source = args[1];
+url = args[2];
+
+gtf <- rtracklayer::import(paste0(url)) 
 gtf_df=as.data.frame(gtf)
 gtf_df_gene=subset(gtf_df,gtf_df$type=="gene")
 
+if(gtf_source=="ensembl"){
+	xanot <- gtf_df_gene[,c("gene_id","gene_name","gene_biotype","seqnames","start","end","strand")]
+}
+if(gtf_source=="gencode"){
+	xanot <- gtf_df_gene[,c("gene_id","gene_name","gene_type","seqnames","start","end","strand")]
+}
 
-xanot <- gtf_df_gene[,c("gene_id","gene_name","gene_biotype","seqnames","start","end","strand")]
 colnames(xanot)[4] <- "chromosome"
+colnames(xanot)[3] <- "gene_biotype"
 colnames(xanot)[1] <- "Geneid"
 
-write.table(xanot, "./annotation/Homo_sapiens.GRCh38.84.chr_complete_annotation.txt", sep="\t", quote=F, row.names=F)
+write.table(xanot, "./annotation/gene_annotation.txt", sep="\t", quote=F, row.names=F)
 ## R script (END)
 ## Please run 2.extract_promoters.sh after this script
--- 2.extract_promoters.sh
+++ 2.extract_promoters.sh
@@ -4,7 +4,9 @@
 #Homo_sapiens.GRCh38.84.chr_complete_annotation.txt : Gene annotations used for quantification purpose (extract from GTF using R script)
 #DE_genes_list_total.txt : Differentially expressed genes for whcih promoter to be extracted
 ##2-step-Run: 
-#./2.extract_promoters.sh prepare_genome
+#./2.extract_promoters.sh prepare_genome https://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz # Ensembl
+#OR
+#./2.extract_promoters.sh prepare_genome https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_25/GRCh38.primary_assembly.genome.fa.gz # Gencode
 #./2.extract_promoters.sh extract protein_coding_Up > sample_output.fasta
 ###########################################################################
 ## Bash script (Start)
@@ -15,26 +17,26 @@ module load SAMtools/1.14-GCC-10.3.0
 
 if [ $1 == "prepare_genome" ]
 then
+	url=$2;
 	echo "## Preparing Genome file. This might take a while";
-	wget https://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz -P ./annotation/ ;
-	gzip -d ./annotation/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz ;
-	mv ./annotation/Homo_sapiens.GRCh38.dna.primary_assembly.fa ./annotation/hg38.fa;
-	#sed -i 's/^>/>chr/g' ./annotation/hg38.fa;
+	wget $url -P ./annotation/ -O ./annotation/genome.fa.gz ;
+	gzip -d ./annotation/genome.fa.gz ;
+	#sed -i 's/^>/>chr/g' ./annotation/genome.fa.gz;
 fi
 
 if [ $1 == "extract" ]
 then
 	
 	group=$2; #protein_coding_Up OR lncRNA_Up OR protein_coding_Down OR lncRNA_Down (USER INPUT NECESSARY)
-	ref="./annotation/hg38.fa"; # Genome used for alignment purpose
-	anot="./annotation/Homo_sapiens.GRCh38.84.chr_complete_annotation.txt"; # Gene annotations used for quantification purpose
+	ref="./annotation/genome.fa"; # Genome used for alignment purpose
+	anot="./annotation/gene_annotation.txt"; # Gene annotations used for quantification purpose
 	DEG="./input/DE_genes_list_total.txt" # Differentially expressed genes for whcih promoter to be extracted
 	size=250; # Number of base paires to extend TSS on both sides. This will extend TSS 250 bo upstream and 250 bp downstream (500 bp).
 
 
 	temp=(`cat $DEG | grep -v "^#" | grep -w $group | cut -f1 > ./input/DE_genes_group.txt`); # Selecting only group of genes of interest (by ENSGxxxx ids)
 	sub_group="./input/DE_genes_group.txt";
-	COLS=(`grep -Ff $sub_group $anot | grep -v "^#" | awk '!x[$0]++' | sed 's/\t/;/g'`); # Selecting complete gene annotations for "lncRNA_Down" genes (by ENSGxxxx ids)
+	COLS=(`grep -w -Ff $sub_group $anot | grep -v "^#" | awk '!x[$0]++' | sed 's/\t/;/g'`); # Selecting complete gene annotations for "lncRNA_Down" genes (by ENSGxxxx ids)
 	
 	rm ./input/DE_genes_group.txt;
 	for i in ${COLS[@]}
@@ -62,7 +64,7 @@ then
 			pend=$(($end+$size));
 		fi
 		samtools faidx --continue $ref $chromosome:$pstart-$pend | sed "s/>$chromosome:$pstart-$pend/>${oname} chr$chromosome:$pstart-$pend/"; #; promoter chr$chromosome:${pstart}-${pend}/"; # if chromosome numbers are 1,2,3 etc.,
-		#samtools faidx --continue $ref chr$chromosome:$pstart-$pend | sed "s/>chr$chromosome:$pstart-$pend/>${oname} chr$chromosome:$pstart-$pend/"; #; promoter chr$chromosome:${pstart}-${pend}/"; # if chromosome numbers are chr1,chr2,chr3 etc.,
+		#samtools faidx --continue $ref chr$chromosome:$pstart-$pend | sed "s/>chr$chromosome:$pstart-$pend/>${oname} $chromosome:$pstart-$pend/"; #; promoter chr$chromosome:${pstart}-${pend}/"; # if chromosome numbers are chr1,chr2,chr3 etc.,
 	done
 	## Bash script (END)
 
